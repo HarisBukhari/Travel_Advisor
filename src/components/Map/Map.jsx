3bbb57f16af0c37a55b23c732e7b74f8703474d6
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2022-07-18 20:30:11 +0500

    Adding

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,37 @@
 import React from "react"
+import GoogleMapReact from 'google-map-react';
+// import { useMediaQuery } from '@material-ui/core';
+// import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+// import Rating from '@material-ui/lab/Rating';
+
+// import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
 
 const Map = () => {
-    return <div>
-        <h1>Map</h1>
-    </div>
+  const classes = useStyles();
+//   const matches = useMediaQuery('(min-width:600px)');
+
+  const coordinates = {lat:0,lng:0}
+    return (
+        <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyAXqL04ILbAhmm3YK6UkvJcSu3ywB5XO7w' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+        // options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+        // onChange={(e) => {
+        //   set coordinates({ lat: e.center.lat, lng: e.center.lng });
+        //   setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        // }}
+        // onChildClick={(child) => setChildClicked(child)}
+      ></GoogleMapReact>
+      </div>
+    )
 }
 
 export default Map
